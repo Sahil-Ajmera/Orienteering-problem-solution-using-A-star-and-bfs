778b2dd9b66a8a1a3f1bf7caabf93f04a37b7d75
Sahil Ajmera <32546795+Sahil-Ajmera@users.noreply.github.com>
2018-03-15 10:33:55 -0400

    Setting predefined values for speed
    Predefined values for speed through different terrains

--- MainFile.py
+++ MainFile.py
@@ -28,6 +28,26 @@ def readImageAndElevationFile(map, elevation_file):
         elevation_info.append(lines.split())
     return map_image, elevation_info, pixels
 
+def set_values_for_speed(speed_through_diff_paths):
+    """
+    
+    :param time_to_go_through_diff_paths: 
+    :return: 
+    """""
+
+    # Speed when going through different types of path
+
+    speed_through_diff_paths[(248, 148, 18)] = 8
+    speed_through_diff_paths[(255, 192, 0)] = 3
+    speed_through_diff_paths[(255, 255, 255)] = 7
+    speed_through_diff_paths[(2, 208, 60)] = 5
+    speed_through_diff_paths[(2, 136, 40)] = 2
+    speed_through_diff_paths[(5, 73, 24)] = 1
+    speed_through_diff_paths[(0, 0, 255)] = 4
+    speed_through_diff_paths[(71, 51, 3)] = 10
+    speed_through_diff_paths[(0, 0, 0)] = 9
+    speed_through_diff_paths[(205, 0, 101)] = 0
+
 
 
 # Map = input('Enter the map file')
